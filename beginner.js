678aeca12b0a37dc2b16571b206b6ce31629eb0d
Aditya Anand <aditechdevofficial@gmail.com>
2023-01-20 09:11:45 +0530

    :memo: Learn javascript

---
+++ beginner.js
@@ -0,0 +1,59 @@
+console.log("Hello World")
+// Inline Comment
+/**
+ * MultiLine Comment
+ * 
+ * 
+ */
+
+/*
+Another Comment
+
+*/
+var number = 5;
+console.log(number.typeOf);
+
+
+/**
+ * DataTypes
+ * 
+ * 7 type of data types
+ * 
+ * undefined, null, boolean, string, symbol, number and obejct
+ */
+
+var myName = "Aditya"; // var can be used globally
+myName = 9;
+
+let ourName = "Anand"; // let can only be used in the 
+
+const pi = 3.14; // Cannot be changed
+
+var a; // Declaring
+
+var b = 2; //Declaring and assigning 
+
+console.log("PRINT"); // print the data
+
+/**
+ * 
+ * Escape Character
+ * 
+ * \' Single quote
+ * \" double quote
+ * \\ back slash
+ * \n new line
+ * \r carriage return
+ * \t tab
+ * \b backspase
+ * \f form feed
+ * 
+ */
+
+// Array
+
+var ourArray = [20, "Aman", ["!", 2, "x"]];
+
+ourArray.pop(); // remove the last item
+
+ourArray.push(["saha"]); // to Update list
